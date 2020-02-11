df88bedacb1a0f2d0bba04e5a8abb4676186b891
Tsering Lama <tseringlama@pursuit.org>
2020-02-11 10:42:10 -0500

    created tab bar controllers with 3 view controllers

--- Unit4Assessment/Unit4Assessment/TabBar/TabBar.swift
+++ Unit4Assessment/Unit4Assessment/TabBar/TabBar.swift
@@ -9,10 +9,27 @@
 import UIKit
 
 class TabBar: UITabBarController {
+    
+    private var cardsVC: CardsVC = {
+       let vc = CardsVC()
+        vc.tabBarItem = UITabBarItem(title: "Cards", image: UIImage(systemName: "creditcard.fill"), tag: 0)
+        return vc
+    }()
+    
+    private var createVC: CreateVC = {
+       let vc = CreateVC()
+        vc.tabBarItem = UITabBarItem(title: "Create", image: UIImage(systemName: "plus.square.fill"), tag: 1)
+        return vc
+    }()
+
+    private var searchVC: SearchVC = {
+        let vc = SearchVC()
+        vc.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
+        return vc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        viewControllers = [cardsVC, createVC, searchVC]
     }
-
 }
